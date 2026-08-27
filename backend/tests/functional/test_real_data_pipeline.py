7c54bbf5d78cc0f635490a73b5d4987e47a53e5e
Aleksandr Titarenko <alekst23@gmail.com>
2026-08-27 12:49:23 -0400

    test(T-1001-9): add failing test stubs for real data pipeline

---
+++ backend/tests/functional/test_real_data_pipeline.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+class TestRealPanelConformance:
+	def test_real_backfill_output_matches_price_bar_schema(self) -> None:
+		pytest.fail("not implemented")
+
+
+class TestAsOfDateVisibility:
+	def test_api_response_includes_panel_as_of_date(self) -> None:
+		pytest.fail("not implemented")
---
+++ backend/tests/unit/test_universe_metadata.py
@@ -0,0 +1,6 @@
+import pytest
+
+
+class TestUniverseMetadataParsing:
+	def test_parse_nasdaq_screener_csv_produces_valid_ticker_metadata(self) -> None:
+		pytest.fail("not implemented")
